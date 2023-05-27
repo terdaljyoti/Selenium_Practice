9befcda7e2f865eb6aac3f507b2938199b7e916c
pravinui <110659009+pravinui@users.noreply.github.com>
2023-05-27 13:30:17 +0530

    [jyoti]:code updated

---
+++ src/WebTable/EX_2.java
@@ -0,0 +1,21 @@
+package WebTable;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class EX_2 
+{
+	public static void main(String[] args)
+	{
+		WebDriver driver =new ChromeDriver();
+		driver.get("https://demo.guru99.com/test/web-table-element.php");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		int RowCount = driver.findElements(By.xpath("//table[@class='dataTable']//tr")).size();
+		System.out.println(RowCount);
+		
+	}
+
+}
